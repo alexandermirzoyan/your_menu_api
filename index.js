e63f555e1050835e1f5326b0cdb11c42761ecc69
Alexandr <alexandr.mirz12@gmail.com>
2021-03-28 23:27:13 +0400

    added cors config for localhost:3000

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require("cors");
 const mongoose = require('mongoose');
 const app = express();
 mongoose.connect('mongodb://localhost/YourMenu', {useNewUrlParser: false});
@@ -6,6 +7,15 @@ const Schema = mongoose.Schema;
 
 const EntitiesModel = mongoose.model('entities', new Schema({name: String}, {collection: 'entities'}));
 const MenuListModel = mongoose.model('menuList', new Schema({name: String}, {collection: 'menuList'}));
+
+const allowList = ["http://localhost:3000"];
+app.use(cors({
+  origin: function (origin, cb) {
+    if (allowList.indexOf(origin) !== -1 || !origin) cb(null, true);
+    else cb(new Error('Not allowed by CORS'));
+  }
+}));
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Unauthorized');
