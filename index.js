fe15883eb36e27393c585cc021117bafe8d642e3
Alexandr <alexandr.mirz12@gmail.com>
2021-04-01 23:46:38 +0400

    added SetOrder API call

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require("cors");
 const mongoose = require('mongoose');
+const nodemailer = require("nodemailer");
+
 const app = express();
 mongoose.connect('mongodb://localhost/YourMenu', {useNewUrlParser: false});
 const Schema = mongoose.Schema;
@@ -31,6 +33,51 @@ app.get('/Entities', (req, res) => {
 app.get('/MenuItems', (req, res) => {
   MenuListModel.findOne({entityId: req.query.entity}, (error, result) => {
     res.send(result);
+  });
+});
+
+app.post('/SetOrder', (req, res) => {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    port: 465,
+    secure: true,
+    auth: {
+      // type: "OAuth2",
+      user: "alexandr.mirz12@gmail.com",// process.env.EMAIL
+      pass: "Alzapet.032002",
+      /*clientId: "1051223923501-3idpg2pfd02buqpu4ipfkdnugsn2p6k6.apps.googleusercontent.com", // process.env.CLIENT_ID
+      clientSecret: "6tbHBKPffODnoAPAGwaHg78D", // process.env.CLIENT_SECRET
+      refreshToken: "1//0fV3ZhQSi5mYwCgYIARAAGA8SNwF-L9IrcPK9PSKabLFmyO6-Fs3WOKqKAp2DNv0uVxZ7tz_B-iNLByJTVl0jX_iqv1jTqinYXzY", // process.env.REFRESH_TOKEN*/
+    },
+  }, /*{
+    rejectUnauthorized: false
+  }*/);
+
+  const mailOptions = {
+    from: "alexandr.mirz12@gmail.com",
+    to: ["alexandr.mirz12@gmail.com"],
+    subject: `Պատվեր 12րդ սեղանից`,
+    text: 'ասդասդադս',
+  };
+
+  transporter.sendMail(mailOptions, (err, data) => {
+    if (!err) {
+      res
+        .status(200)
+        .send({
+          resultCode: 1,
+          result: "Պատվերը հաստատվեց!"
+        });
+    }
+    else {
+      console.log(err);
+      res
+        .send(200)
+        .status({
+          resultCode: 2,
+          result: "Կապի խնդիր"
+        })
+    }
   });
 });
 
